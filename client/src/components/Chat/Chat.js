654085e3b1c8d3be33dc506af13d2a236b323227
ritesh <rg.riteshgoel57@gmail.com>
2021-02-19 15:24:19 +0530

    ready

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from "socket.io-client";
 
-// import TextContainer from '../TextContainer/TextContainer';
-//import Messages from '../Messages/Messages';
-//import InfoBar from '../InfoBar/InfoBar';
-//import Input from '../Input/Input';
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 import './Chat.css';
 
@@ -17,8 +17,8 @@ const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
   const [users, setUsers] = useState('');
-  // const [message, setMessage] = useState('');
-  // const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -28,21 +28,16 @@ const Chat = ({ location }) => {
     setRoom(room);
     setName(name)
 
-    socket.emit('join', { name, room }, () => {
-      // if(error) {
-      //   alert(error);
-      // }
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
     });
-
-    return () =>{
-      socket.emit('disconnect');
-      socket.off();
-    }
   }, [ENDPOINT, location.search]);
   
   useEffect(() => {
     socket.on('message', message => {
-      // setMessages(messages => [ ...messages, message ]);
+      setMessages(messages => [ ...messages, message ]);
     });
     
     socket.on("roomData", ({ users }) => {
@@ -53,19 +48,19 @@ const Chat = ({ location }) => {
   const sendMessage = (event) => {
     event.preventDefault();
 
-    // if(message) {
-    //   socket.emit('sendMessage', message, () => setMessage(''));
-    // }
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
   }
 
   return (
     <div className="outerContainer">
       <div className="container">
-          {/* <InfoBar room={room} /> */}
-          {/* <Messages messages={messages} name={name} /> */}
-          {/* <Input message={message} setMessage={setMessage} sendMessage={sendMessage} /> */}
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
-      {/* <TextContainer users={users}/> */}
+      <TextContainer users={users}/>
     </div>
   );
 }
